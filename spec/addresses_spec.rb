b127eb39df91f61ec51203a8c3cf78be2feb9b9d
awwa500@gmail.com <awwa500@gmail.com>
2014-10-24 23:39:15 +0900

    Update addresses_spec

--- spec/addresses_spec.rb
+++ spec/addresses_spec.rb
@@ -7,41 +7,43 @@ describe "SendGrid4r::REST::Ips::Addresses" do
     Dotenv.load
   end
 
-  context "free account" do
-    it "raise error for get_ips" do
-
-      begin
-        client = SendGrid4r::Client.new(ENV["SENDGRID_USERNAME"], ENV["SENDGRID_PASSWORD"])
-        # get the ips
-        expect{client.get_ips}.to raise_error(RestClient::Forbidden)
-        # get the ip
-        expect{client.get_ip("10.10.10.10").to raise_error(RestClient::Forbidden)}
-      rescue => ex
-        puts ex.inspect
-        raise ex
-      end
-
+  context "if account is free" do
+    before :all do
+      @client = SendGrid4r::Client.new(ENV["SENDGRID_USERNAME"], ENV["SENDGRID_PASSWORD"])
     end
 
+    describe "#get_ips" do
+      it "raise error" do
+        expect{@client.get_ips}.to raise_error(RestClient::Forbidden)
+      end
+    end
+
+    describe "#get_ip" do
+      it "raise error" do
+        expect{@client.get_ip("10.10.10.10").to raise_error(RestClient::Forbidden)}
+      end
+    end
   end
 
-  context "silver account" do
-    it "returns Array of Address instance" do
+  context "if account is silver" do
+    before :all do
+      @client = SendGrid4r::Client.new(ENV["SILVER_SENDGRID_USERNAME"], ENV["SILVER_SENDGRID_PASSWORD"])
+    end
 
-      begin
-        client = SendGrid4r::Client.new(ENV["SILVER_SENDGRID_USERNAME"], ENV["SILVER_SENDGRID_PASSWORD"])
-        # get the ips
-        ips = client.get_ips
-        expect(ips.length).to be(1)
+    describe "#get_ips" do
+      it "returns Array of Address instance" do
+        ips = @client.get_ips
+        expect(ips.length > 0).to be(true)
         expect(ips[0].class).to be(SendGrid4r::REST::Ips::Address)
-        expect(client.get_ip(ips[0].ip).class).to be(SendGrid4r::REST::Ips::Address)
-      rescue => ex
-        puts ex.inspect
-        raise ex
       end
+    end
 
+    describe "#get_ip" do
+      it "returns Address instance" do
+        ips = @client.get_ips
+        expect(@client.get_ip(ips[0].ip).class).to be(SendGrid4r::REST::Ips::Address)
+      end
     end
 
   end
-
 end
