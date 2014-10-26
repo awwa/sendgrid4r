32310f585de4607954b8759aa77b0f0e82103ed4
awwa500@gmail.com <awwa500@gmail.com>
2014-10-26 22:09:27 +0900

    Improve version_factory_spec

--- spec/templates_spec.rb
+++ spec/templates_spec.rb
@@ -3,51 +3,37 @@ require File.dirname(__FILE__) + '/spec_helper'
 
 describe "SendGrid4r::REST::Templates" do
 
+  TEMPLATE_NAME = "template_test"
+  TEMPLATE_EDIT = "template_edit"
+
   before :all do
     Dotenv.load
-    @template_name = "template_test"
-    @template_edit = "template_edit"
+    @client = SendGrid4r::Client.new(ENV["SENDGRID_USERNAME"], ENV["SENDGRID_PASSWORD"])
   end
 
-  context "normal" do
+  context "always" do
     it "is normal" do
-
-      begin
-        client = SendGrid4r::Client.new(ENV["SENDGRID_USERNAME"], ENV["SENDGRID_PASSWORD"])
-        # celan up test env
-        tmps = client.get_templates
-        expect(tmps.length >= 0).to eq(true)
-        tmps.each{|tmp|
-          if tmp.name == @template_name || tmp.name == @template_edit then
-            client.delete_template(tmp.id)
-          end
-        }
-        # post a template
-        new_template = client.post_template(@template_name)
-        expect(@template_name).to eq(new_template.name)
-        # pach the template
-        client.patch_template(new_template.id, @template_edit)
-        # get the template
-        edit_template = client.get_template(new_template.id)
-        expect(new_template.id).to eq(edit_template.id)
-        expect(@template_edit).to eq(edit_template.name)
-        expect(new_template.versions).to eq(edit_template.versions)
-        # delete the template
-        client.delete_template(edit_template.id)
-        expect{client.get_template(edit_template.id)}.to raise_error(RestClient::ResourceNotFound)
-      rescue => ex
-        puts ex.inspect
-        raise ex
-      end
-
+      # celan up test env
+      tmps = @client.get_templates
+      expect(tmps.length >= 0).to eq(true)
+      tmps.each{|tmp|
+        if tmp.name == TEMPLATE_NAME || tmp.name == TEMPLATE_EDIT then
+          @client.delete_template(tmp.id)
+        end
+      }
+      # post a template
+      new_template = @client.post_template(TEMPLATE_NAME)
+      expect(TEMPLATE_NAME).to eq(new_template.name)
+      # pach the template
+      @client.patch_template(new_template.id, TEMPLATE_EDIT)
+      # get the template
+      edit_template = @client.get_template(new_template.id)
+      expect(new_template.id).to eq(edit_template.id)
+      expect(TEMPLATE_EDIT).to eq(edit_template.name)
+      expect(new_template.versions).to eq(edit_template.versions)
+      # delete the template
+      @client.delete_template(edit_template.id)
+      expect{@client.get_template(edit_template.id)}.to raise_error(RestClient::ResourceNotFound)
     end
   end
-
-  context "abnormal" do
-    it "raise resource not found for none existance id" do
-      client = SendGrid4r::Client.new(ENV["SENDGRID_USERNAME"], ENV["SENDGRID_PASSWORD"])
-      expect{client.get_template("notexistid")}.to raise_error(RestClient::ResourceNotFound)
-    end
-  end
-
 end
--- spec/version_factory_spec.rb
+++ spec/version_factory_spec.rb
@@ -5,33 +5,26 @@ describe "SendGrid4r::VersionFactory" do
 
   before :all do
     Dotenv.load
+    @factory = SendGrid4r::VersionFactory.new
   end
 
-  context "normal" do
+  context "always" do
     it "is simple case" do
-
-      factory = SendGrid4r::VersionFactory.new
-      version = factory.create("version_name")
+      version = @factory.create("version_name")
       expect(version.name).to eq("version_name")
       expect(version.subject).to eq("<%subject%>")
       expect(version.html_content).to eq("<%body%>")
       expect(version.plain_content).to eq("<%body%>")
       expect(version.active).to eq(1)
-
     end
 
     it "is full params case" do
-
-      factory = SendGrid4r::VersionFactory.new
-      version = factory.create("version_name", "This is subject <%subject%>", "This is html content <%body%>", "This is plain content <%body%>", 0)
+      version = @factory.create("version_name", "This is subject <%subject%>", "This is html content <%body%>", "This is plain content <%body%>", 0)
       expect(version.name).to eq("version_name")
       expect(version.subject).to eq("This is subject <%subject%>")
       expect(version.html_content).to eq("This is html content <%body%>")
       expect(version.plain_content).to eq("This is plain content <%body%>")
       expect(version.active).to eq(0)
-
     end
-
   end
-
 end
