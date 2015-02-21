a1e5898232da1f57cd2c9ca12fe53a803a342f71
awwa500@gmail.com <awwa500@gmail.com>
2015-02-22 01:53:43 +0900

    Revert to version

--- lib/sendgrid4r.rb
+++ lib/sendgrid4r.rb
@@ -2,10 +2,4 @@
 $LOAD_PATH.unshift File.dirname(__FILE__)
 
 require 'client'
-
-# SendGrid API v3 wrapper implementation.
-#
-#
-module SendGrid4r
-  VERSION = '0.1.0'
-end
+require "sendgrid4r/version"
---
+++ lib/sendgrid4r/version.rb
@@ -0,0 +1,6 @@
+#
+# SendGrid API v3 wrapper implementation.
+#
+module SendGrid4r
+  VERSION = '0.1.0'
+end
