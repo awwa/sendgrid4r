0169622af86c114766e66494f6e232ecd449c37e
awwa500@gmail.com <awwa500@gmail.com>
2015-04-18 07:05:10 +0900

    Update version_spec

--- spec/templates/versions_spec.rb
+++ spec/templates/versions_spec.rb
@@ -7,79 +7,79 @@ describe 'SendGrid4r::REST::Templates::Versions' do
     @client = SendGrid4r::Client.new(
       ENV['SENDGRID_USERNAME'], ENV['SENDGRID_PASSWORD']
     )
-    @template_edit = 'version_test'
-    @version1_name = 'version1_test'
-    @version2_name = 'version2_test'
+    @template_name = 'version_test'
+    @version1_name = 'version_name1'
+    @version2_name = 'version_name2'
+    @factory = SendGrid4r::Factory::VersionFactory.new
   end
 
-  context 'always' do
-    it 'is normal' do
+  context 'without block call' do
+    before :all do
       begin
         # celan up test env
         tmps = @client.get_templates
         tmps.templates.each do |tmp|
-          next if tmp.name != @template_edit
+          next if tmp.name != @template_name
           tmp.versions.each do |ver|
             @client.delete_version(tmp.id, ver.id)
           end
           @client.delete_template(tmp.id)
         end
         # post a template
-        new_template = @client.post_template(@template_edit)
-        expect(@template_edit).to eq(new_template.name)
+        @template = @client.post_template(@template_name)
         # post a version
-        factory = SendGrid4r::Factory::VersionFactory.new
-        ver1 = factory.create(name: @version1_name)
-        ver1 = @client.post_version(new_template.id, ver1)
-        # get the version
-        actual = @client.get_version(new_template.id, ver1.id)
-        expect(ver1.template_id).to eq(actual.template_id)
-        expect(ver1.active).to eq(actual.active)
-        expect(ver1.name).to eq(actual.name)
-        expect(ver1.html_content).to eq(actual.html_content)
-        expect(ver1.plain_content).to eq(actual.plain_content)
-        expect(ver1.subject).to eq(actual.subject)
-        # edit the version
-        edit_ver1 = actual.dup
+        ver1 = @factory.create(name: @version1_name)
+        @version1 = @client.post_version(@template.id, ver1)
+      rescue => e
+        puts e.inspect
+        raise e
+      end
+    end
+
+    it '#post_version' do
+      begin
+        ver2 = @factory.create(name: @version2_name)
+        version2 = @client.post_version(@template.id, ver2)
+        expect(version2.name).to eq(@version2_name)
+      rescue => e
+        puts e.inspect
+        raise e
+      end
+    end
+
+    it '#get_version' do
+      begin
+        actual = @client.get_version(@template.id, @version1.id)
+        expect(actual.template_id).to eq(@version1.template_id)
+        expect(actual.active).to be_a(Fixnum)
+        expect(actual.name).to eq(@version1.name)
+        expect(actual.html_content).to eq(@version1.html_content)
+        expect(actual.plain_content).to eq(@version1.plain_content)
+        expect(actual.subject).to eq(@version1.subject)
+      rescue => e
+        puts e.inspect
+        raise e
+      end
+    end
+
+    it '#patch_version' do
+      begin
+        edit_ver1 = @version1.dup
         edit_ver1.name = 'edit_version'
         edit_ver1.subject = 'edit<%subject%>edit'
         edit_ver1.html_content = 'edit<%body%>edit'
         edit_ver1.plain_content = 'edit<%body%>edit'
         edit_ver1.active = 0
-        @client.patch_version(new_template.id, ver1.id, edit_ver1)
-        # get the version
-        actual = @client.get_version(new_template.id, ver1.id)
-        expect(new_template.id).to eq(actual.template_id)
-        expect(edit_ver1.active).to eq(actual.active)
-        expect(edit_ver1.name).to eq(actual.name)
-        expect(edit_ver1.html_content).to eq(actual.html_content)
-        expect(edit_ver1.plain_content).to eq(actual.plain_content)
-        expect(edit_ver1.subject).to eq(actual.subject)
-        # post a version 2
-        ver2 = factory.create(
-          name: @version2_name,
-          subject: '<%subject%>',
-          html_content: '<%body%>',
-          plain_content: '<%body%>'
-        )
-        ver2 = @client.post_version(new_template.id, ver2)
-        # activate version 2
-        @client.activate_version(new_template.id, ver2.id)
-        actual_ver1 = @client.get_version(new_template.id, ver1.id)
-        actual_ver2 = @client.get_version(new_template.id, ver2.id)
-        expect(0).to eq(actual_ver1.active)
-        expect(1).to eq(actual_ver2.active)
-        # delete the version
-        @client.delete_version(new_template.id, actual_ver1.id)
-        @client.delete_version(new_template.id, actual_ver2.id)
-        expect do
-          @client.get_version(new_template.id, actual_ver1.id)
-        end.to raise_error(RestClient::ResourceNotFound)
-        expect do
-          @client.get_version(new_template.id, actual_ver2.id)
-        end.to raise_error(RestClient::ResourceNotFound)
-        # delete the template
-        @client.delete_template(new_template.id)
+        @client.patch_version(@template.id, @version1.id, edit_ver1)
+      rescue => e
+        puts e.inspect
+        raise e
+      end
+    end
+
+    it '#delete_version' do
+      begin
+        @client.delete_version(@template.id, @version1.id)
       rescue => e
         puts e.inspect
         raise e
