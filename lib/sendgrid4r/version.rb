5f8af47e8e05552498949796d4703bce59d26636
awwa500@gmail.com <awwa500@gmail.com>
2016-02-10 22:20:07 +0900

    version bump 1.5.1

--- lib/sendgrid4r/version.rb
+++ lib/sendgrid4r/version.rb
@@ -2,5 +2,5 @@
 # SendGrid API v3 wrapper implementation.
 #
 module SendGrid4r
-  VERSION = '1.5.0'
+  VERSION = '1.5.1'
 end
--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -282,7 +282,7 @@ describe SendGrid4r::Client do
 
     describe 'VERSION' do
       it 'returns VERSION value' do
-        expect(SendGrid4r::VERSION).to eq('1.5.0')
+        expect(SendGrid4r::VERSION).to eq('1.5.1')
       end
     end
   end
