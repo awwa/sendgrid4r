d48c19c224fd3c4b6c3adf58516040463d98c5ed
awwa500@gmail.com <awwa500@gmail.com>
2016-04-21 21:20:20 +0900

    version bump 1.8.1

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -313,7 +313,7 @@ describe SendGrid4r::Client do
 
     describe 'VERSION' do
       it 'returns VERSION value' do
-        expect(SendGrid4r::VERSION).to eq('1.8.0')
+        expect(SendGrid4r::VERSION).to eq('1.8.1')
       end
     end
   end
