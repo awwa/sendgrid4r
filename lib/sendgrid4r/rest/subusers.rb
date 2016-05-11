368a0ff70a765c02a0022252f3ebd71509a028a1
awwa500@gmail.com <awwa500@gmail.com>
2016-05-11 22:24:09 +0900

    refactor bounces, subusers, users

--- lib/sendgrid4r/rest/subusers.rb
+++ lib/sendgrid4r/rest/subusers.rb
@@ -1,140 +1,118 @@
 # -*- encoding: utf-8 -*-
 
-module SendGrid4r
-  module REST
-    #
-    # SendGrid Web API v3 Subusers
-    #
-    module Subusers
-      include SendGrid4r::REST::Request
+module SendGrid4r::REST
+  #
+  # SendGrid Web API v3 Subusers
+  #
+  module Subusers
+    include Request
 
-      Subuser = Struct.new(
-        :id, :username, :email, :password, :ips, :reputation, :disabled
+    Subuser = Struct.new(
+      :id, :username, :email, :password, :ips, :reputation, :disabled
+    )
+    Ips = Struct.new(:ips)
+
+    def self.url(subuser_name = nil)
+      url = "#{BASE_URL}/subusers"
+      url = "#{url}/#{subuser_name}" unless subuser_name.nil?
+      url
+    end
+
+    def self.create_subusers(resp)
+      return resp if resp.nil?
+      subusers = []
+      resp.each do |subuser|
+        subusers.push(Subusers.create_subuser(subuser))
+      end
+      subusers
+    end
+
+    def self.create_subuser(resp)
+      return resp if resp.nil?
+      Subuser.new(
+        resp['id'],
+        resp['username'],
+        resp['email'],
+        resp['password'],
+        resp['ips'],
+        resp['reputation'],
+        resp['disabled']
       )
-      Ips = Struct.new(:ips)
+    end
 
-      def self.url(subuser_name = nil)
-        url = "#{BASE_URL}/subusers"
-        url = "#{url}/#{subuser_name}" unless subuser_name.nil?
-        url
-      end
+    Monitor = Struct.new(:email, :frequency)
 
-      def self.create_subusers(resp)
-        return resp if resp.nil?
-        subusers = []
-        resp.each do |subuser|
-          subusers.push(SendGrid4r::REST::Subusers.create_subuser(subuser))
-        end
-        subusers
-      end
+    def self.create_monitor(resp)
+      return resp if resp.nil?
+      Monitor.new(resp['email'], resp['frequency'])
+    end
 
-      def self.create_subuser(resp)
-        return resp if resp.nil?
-        Subuser.new(
-          resp['id'],
-          resp['username'],
-          resp['email'],
-          resp['password'],
-          resp['ips'],
-          resp['reputation'],
-          resp['disabled']
-        )
-      end
+    def get_subusers(limit: nil, offset: nil, username: nil, &block)
+      params = {}
+      params['limit'] = limit unless limit.nil?
+      params['offset'] = offset unless offset.nil?
+      params['username'] = username unless username.nil?
+      Subusers.create_subusers(get(@auth, Subusers.url, params, &block))
+    end
 
-      Monitor = Struct.new(:email, :frequency)
+    def post_subuser(username:, email:, password:, ips:, &block)
+      params = {}
+      params['username'] = username
+      params['email'] = email
+      params['password'] = password
+      params['ips'] = ips
+      Subusers.create_subuser(post(@auth, Subusers.url, params, &block))
+    end
 
-      def self.create_monitor(resp)
-        return resp if resp.nil?
-        Monitor.new(
-          resp['email'],
-          resp['frequency']
-        )
-      end
+    def patch_subuser(username:, disabled:, &block)
+      payload = {}
+      payload['disabled'] = disabled
+      resp = patch(@auth, Subusers.url(username), payload, &block)
+      Subusers.create_subuser(resp)
+    end
 
-      def get_subusers(limit: nil, offset: nil, username: nil, &block)
-        params = {}
-        params['limit'] = limit unless limit.nil?
-        params['offset'] = offset unless offset.nil?
-        params['username'] = username unless username.nil?
-        resp = get(@auth, SendGrid4r::REST::Subusers.url, params, &block)
-        SendGrid4r::REST::Subusers.create_subusers(resp)
-      end
+    def delete_subuser(username:, &block)
+      delete(@auth, Subusers.url(username), &block)
+    end
 
-      def post_subuser(username:, email:, password:, ips:, &block)
-        params = {}
-        params['username'] = username
-        params['email'] = email
-        params['password'] = password
-        params['ips'] = ips
-        resp = post(@auth, SendGrid4r::REST::Subusers.url, params, &block)
-        SendGrid4r::REST::Subusers.create_subuser(resp)
-      end
+    def get_subuser_monitor(username:, email:, frequency:, &block)
+      endpoint = "#{Subusers.url(username)}/monitor"
+      payload = {}
+      payload['email'] = email
+      payload['frequency'] = frequency
+      Subusers.create_monitor(post(@auth, endpoint, payload, &block))
+    end
 
-      def patch_subuser(username:, disabled:, &block)
-        endpoint = SendGrid4r::REST::Subusers.url(username)
-        payload = {}
-        payload['disabled'] = disabled
-        resp = patch(@auth, endpoint, payload, &block)
-        SendGrid4r::REST::Subusers.create_subuser(resp)
-      end
+    def post_subuser_monitor(username:, email:, frequency:, &block)
+      endpoint = "#{Subusers.url(username)}/monitor"
+      payload = {}
+      payload['email'] = email
+      payload['frequency'] = frequency
+      Subusers.create_monitor(post(@auth, endpoint, payload, &block))
+    end
 
-      def delete_subuser(username:, &block)
-        endpoint = SendGrid4r::REST::Subusers.url(username)
-        delete(@auth, endpoint, &block)
-      end
+    def put_subuser_monitor(username:, email:, frequency:, &block)
+      endpoint = "#{Subusers.url(username)}/monitor"
+      payload = {}
+      payload['email'] = email
+      payload['frequency'] = frequency
+      Subusers.create_monitor(put(@auth, endpoint, payload, &block))
+    end
 
-      def get_subuser_monitor(username:, email:, frequency:, &block)
-        endpoint = SendGrid4r::REST::Subusers.url(username)
-        endpoint = "#{endpoint}/monitor"
-        payload = {}
-        payload['email'] = email
-        payload['frequency'] = frequency
-        resp = post(@auth, endpoint, payload, &block)
-        SendGrid4r::REST::Subusers.create_monitor(resp)
-      end
+    def delete_subuser_monitor(username:, &block)
+      delete(@auth, "#{Subusers.url(username)}/monitor", &block)
+    end
 
-      def post_subuser_monitor(username:, email:, frequency:, &block)
-        endpoint = SendGrid4r::REST::Subusers.url(username)
-        endpoint = "#{endpoint}/monitor"
-        payload = {}
-        payload['email'] = email
-        payload['frequency'] = frequency
-        resp = post(@auth, endpoint, payload, &block)
-        SendGrid4r::REST::Subusers.create_monitor(resp)
-      end
+    def get_subuser_reputation(usernames:, &block)
+      params = ''
+      usernames.each {|username| params += "usernames=#{username}&"}
+      endpoint = "#{Subusers.url}/reputations?#{params}"
+      Subusers.create_subusers(get(@auth, endpoint, usernames, &block))
+    end
 
-      def put_subuser_monitor(username:, email:, frequency:, &block)
-        endpoint = SendGrid4r::REST::Subusers.url(username)
-        endpoint = "#{endpoint}/monitor"
-        payload = {}
-        payload['email'] = email
-        payload['frequency'] = frequency
-        resp = put(@auth, endpoint, payload, &block)
-        SendGrid4r::REST::Subusers.create_monitor(resp)
-      end
-
-      def delete_subuser_monitor(username:, &block)
-        endpoint = SendGrid4r::REST::Subusers.url(username)
-        endpoint = "#{endpoint}/monitor"
-        delete(@auth, endpoint, &block)
-      end
-
-      def get_subuser_reputation(usernames:, &block)
-        params = ''
-        usernames.each do |username|
-          params += "usernames=#{username}&"
-        end
-        endpoint = SendGrid4r::REST::Subusers.url
-        endpoint = "#{endpoint}/reputations?#{params}"
-        resp = get(@auth, endpoint, usernames, &block)
-        SendGrid4r::REST::Subusers.create_subusers(resp)
-      end
-
-      def put_subuser_assigned_ips(username:, ips:, &block)
-        endpoint = "#{SendGrid4r::REST::Subusers.url(username)}/ips"
-        resp = put(@auth, endpoint, ips, &block)
-        SendGrid4r::REST::Subusers.create_subuser(resp)
-      end
+    def put_subuser_assigned_ips(username:, ips:, &block)
+      resp = put(@auth, "#{Subusers.url(username)}/ips", ips, &block)
+      Subusers.create_subuser(resp)
     end
   end
 end
--- lib/sendgrid4r/rest/users.rb
+++ lib/sendgrid4r/rest/users.rb
@@ -1,64 +1,61 @@
 # -*- encoding: utf-8 -*-
 
-module SendGrid4r
-  module REST
-    #
-    # SendGrid Web API v3 Users
-    #
-    module Users
-      include SendGrid4r::REST::Request
+module SendGrid4r::REST
+  #
+  # SendGrid Web API v3 Users
+  #
+  module Users
+    include Request
 
-      Profile = Struct.new(
-        :address, :city, :company, :country, :first_name, :last_name,
-        :phone, :state, :website, :zip
+    Profile = Struct.new(
+      :address, :city, :company, :country, :first_name, :last_name,
+      :phone, :state, :website, :zip
+    )
+
+    Account = Struct.new(:type, :reputation)
+
+    def self.url(path)
+      "#{BASE_URL}/user/#{path}"
+    end
+
+    def self.create_profile(resp)
+      return resp if resp.nil?
+      Profile.new(
+        resp['address'],
+        resp['city'],
+        resp['company'],
+        resp['country'],
+        resp['first_name'],
+        resp['last_name'],
+        resp['phone'],
+        resp['state'],
+        resp['website'],
+        resp['zip']
       )
+    end
 
-      Account = Struct.new(:type, :reputation)
+    def self.create_account(resp)
+      return resp if resp.nil?
+      Account.new(
+        resp['type'],
+        resp['reputation']
+      )
+    end
 
-      def self.url(path)
-        url = "#{BASE_URL}/user/#{path}"
-        url
-      end
+    def get_user_profile(&block)
+      resp = get(@auth, Users.url('profile'), nil, &block)
+      Users.create_profile(resp)
+    end
 
-      def self.create_profile(resp)
-        return resp if resp.nil?
-        Profile.new(
-          resp['address'],
-          resp['city'],
-          resp['company'],
-          resp['country'],
-          resp['first_name'],
-          resp['last_name'],
-          resp['phone'],
-          resp['state'],
-          resp['website'],
-          resp['zip']
-        )
-      end
+    def patch_user_profile(params:, &block)
+      endpoint = Users.url('profile')
+      resp = patch(@auth, endpoint, params, &block)
+      Users.create_profile(resp)
+    end
 
-      def self.create_account(resp)
-        return resp if resp.nil?
-        Account.new(
-          resp['type'],
-          resp['reputation']
-        )
-      end
-
-      def get_user_profile(&block)
-        resp = get(@auth, SendGrid4r::REST::Users.url('profile'), nil, &block)
-        SendGrid4r::REST::Users.create_profile(resp)
-      end
-
-      def patch_user_profile(params:, &block)
-        endpoint = SendGrid4r::REST::Users.url('profile')
-        resp = patch(@auth, endpoint, params, &block)
-        SendGrid4r::REST::Users.create_profile(resp)
-      end
-
-      def get_user_account(&block)
-        resp = get(@auth, SendGrid4r::REST::Users.url('account'), nil, &block)
-        SendGrid4r::REST::Users.create_account(resp)
-      end
+    def get_user_account(&block)
+      resp = get(@auth, Users.url('account'), nil, &block)
+      Users.create_account(resp)
     end
   end
 end
